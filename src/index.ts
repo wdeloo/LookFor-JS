5da78037ecb072e8391439f9eb77ef86d6e19059
wDeloo <wdelo.hack@gmail.com>
2024-10-21 22:30:07 +0200

    function working, missing some features

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,35 @@ interface props {
     [key: string]: string
 }
 
-export function highlight(fullStr: string, strToHighlight: string, props: props) {
+interface params {
+    keySensitive?: boolean,
+    detectAccents?: boolean
+}
+
+function formatStr(str: string, params: params) {
+    const keySensitive: boolean = params?.keySensitive === undefined ? true : params?.keySensitive
+    const detectAccents: boolean = params?.detectAccents === undefined ? true : params?.detectAccents
+
+    let formattedStr = str
+    if (!keySensitive) formattedStr = formattedStr.toLowerCase()
+    if (!detectAccents) formattedStr = formattedStr.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+
+    return formattedStr
+}
+
+function getAllPositions(fullStr: string, query: string) {
+    const positions = []
+    let position = fullStr.indexOf(query)
+
+    while (position !== -1) {
+        positions.push({start: position, end: position + query.length})
+        position = fullStr.indexOf(query, position + query.length)
+    }
+
+    return positions
+}
+
+export function highlight(fullStr: string, query: string, props: props, params?: params) {
     const valProp = /^[a-zA-Z0-9\-]+$/
 
     if (!props.tag) throw new Error("Missing 'tag' parameter") // tag is compulsory
@@ -16,29 +44,24 @@ export function highlight(fullStr: string, strToHighlight: string, props: props)
         propsString += ` ${propName}="${props[propName]}"` // is the string containing the props, to the right of the tag name in html.
     }
 
-    if (!fullStr.includes(strToHighlight)) return fullStr
+    const formattedFullStr = params ? formatStr(fullStr, params) : fullStr
+    const formattedQuery = params ? formatStr(query, params) : query
+
+    const allPositions = getAllPositions(formattedFullStr, formattedQuery)
+    if (allPositions.length === 0) return fullStr
+
+    allPositions.sort((a, b) => b.start - a.start)
 
     const tag = {
         open: `<${props.tag}${propsString}>`,
         close: `</${props.tag}>`
     }
 
-    const splitString = fullStr.split(strToHighlight)
-
-    let result = ""
-    for (const i in splitString) {
-        const piece = splitString[i]
-        const index = Number(i)
-
-        if (result) result += tag.close // if it is not the first piece of string it will close the tag of the previous one
-
-        result += piece
-
-        if (splitString[index + 1] !== undefined) { // if it is not the last one it will open a string for the next one just before the string the user want to highligh
-            result += tag.open
-            result += strToHighlight
-        }
-    }
+    let result = fullStr
+    allPositions.forEach((position) => {
+        result = result.slice(0, position.end) + tag.close + result.slice(position.end)
+        result = result.slice(0, position.start) + tag.open + result.slice(position.start)
+    })
 
     return result
 }
