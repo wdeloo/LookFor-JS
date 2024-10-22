4d209b94ab592a87533b4bc31c55043280780b01
wDeloo <wdelo.hack@gmail.com>
2024-10-22 19:25:03 +0200

    constructor

--- src/index.ts
+++ src/index.ts
@@ -30,38 +30,49 @@ function getAllPositions(fullStr: string, query: string) {
 
     return positions
 }
+        
+export class LookFor {
+    props: props
+    params: params | undefined
 
-export function highlight(fullStr: string, query: string, props: props, params?: params) {
-    const valProp = /^[a-zA-Z0-9\-]+$/
-
-    if (!props.tag) throw new Error("Missing 'tag' parameter") // tag is compulsory
-
-    let propsString = ""
-    for (const propName in props) {
-        if (propName === "tag") continue // tag is not really a prop, so it must not be shown in the "propsString"
-        if (!valProp.test(propName)) throw new Error(`Invalid prop name "${propName}"`) // check that props have a valid name
-
-        propsString += ` ${propName}="${props[propName]}"` // is the string containing the props, to the right of the tag name in html.
+    constructor(props: props, params?: params) {
+        this.props = props
+        this.params = params
     }
 
-    const formattedFullStr = params ? formatStr(fullStr, params) : fullStr
-    const formattedQuery = params ? formatStr(query, params) : query
-
-    const allPositions = getAllPositions(formattedFullStr, formattedQuery)
-    if (allPositions.length === 0) return fullStr
-
-    allPositions.sort((a, b) => b.start - a.start)
-
-    const tag = {
-        open: `<${props.tag}${propsString}>`,
-        close: `</${props.tag}>`
+    highlight(fullStr: string, query: string, index?: number): string {
+        const valProp = /^[a-zA-Z0-9\-]+$/
+    
+        if (!this.props.tag) throw new Error("Missing 'tag' parameter") // tag is compulsory
+    
+        let propsString = ""
+        for (const propName in this.props) {
+            if (propName === "tag") continue // tag is not really a prop, so it must not be shown in the "propsString"
+            if (!valProp.test(propName)) throw new Error(`Invalid prop name "${propName}"`) // check that props have a valid name
+    
+            propsString += ` ${propName}="${this.props[propName]}"` // is the string containing the props, to the right of the tag name in html.
+        }
+    
+        const formattedFullStr = this.params ? formatStr(fullStr, this.params) : fullStr
+        const formattedQuery = this.params ? formatStr(query, this.params) : query
+    
+        let allPositions = getAllPositions(formattedFullStr, formattedQuery)
+        allPositions = index !== undefined ? [allPositions[index]] : allPositions
+        if (allPositions.length === 0) return fullStr
+    
+        allPositions.sort((a, b) => b.start - a.start)
+    
+        const tag = {
+            open: `<${this.props.tag}${propsString}>`,
+            close: `</${this.props.tag}>`
+        }
+    
+        let result = fullStr
+        allPositions.forEach((position) => {
+            result = result.slice(0, position.end) + tag.close + result.slice(position.end)
+            result = result.slice(0, position.start) + tag.open + result.slice(position.start)
+        })
+    
+        return result
     }
-
-    let result = fullStr
-    allPositions.forEach((position) => {
-        result = result.slice(0, position.end) + tag.close + result.slice(position.end)
-        result = result.slice(0, position.start) + tag.open + result.slice(position.start)
-    })
-
-    return result
 }
